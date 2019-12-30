8d39ddb67e50b265dc6d89b90d3f00bdcdf0eccb
Babatunde Alaraje <alarajebabatunde@gmail.com>
2019-12-30 14:11:37 -0600

    first commit

--- app/index.js
+++ app/index.js
@@ -1,14 +1,12 @@
 const React = require("react");
 const ReactDOM = require("react-dom");
 import "./index.css";
-import Battle from "./components/Battle";
-import Popular from "./components/Popular";
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <Popular />
+        <h1>Hello Babarajkeepers</h1>
       </div>
     );
   }
