11aea3fb8ec686eb0ea146ed5ff6296433cf97e0
rpadi2 <rpadi2@monsanto.com>
2019-12-30 17:02:45 -0600

    changes

--- app/src/containers/MainContainer.js
+++ app/src/containers/MainContainer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {createMuiTheme} from '@material-ui/core/styles';
 import red from '@material-ui/core/colors/red';
 import {ThemeProvider} from '@material-ui/core/styles';
-import HomeContainer from "HomeContainer"
+import HomeContainer from "./HomeContainer"
 
 
 const theme = createMuiTheme({
